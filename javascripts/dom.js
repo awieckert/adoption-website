2d62f24b0d1e7ffed1f1346ee01868a47e61c749
Adam Wieckert <wiec1369@gmail.com>
2018-04-14 14:01:44 -0500

    Have the grid system I want.

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -6,13 +6,15 @@ const printToDom = (pets) => {
 
 const domString = (petsArray) => {
   let stringToPrint = '';
-  petsArray.forEach((pet) => {
-    stringToPrint += `<div class='${pet.type} col-md-4'>`;
+  petsArray.forEach((pet, index) => {
+    stringToPrint += `<div class='col-md-4'>`;
+    stringToPrint += `<div class='${pet.type} pet-card'>`;
     stringToPrint +=  `<h2>${pet.name}</h2>`;
     stringToPrint +=  `<img src='${pet.imageUrl}'>`;
     stringToPrint +=  `<h3>${pet.color}</h3>`;
     stringToPrint +=  `<h4>${pet.specialSkill}</h4>`;
     stringToPrint +=  `<h4>${pet.type.toUpperCase()}</h4>`;
+    stringToPrint += `</div>`;
     stringToPrint += `</div>`;
   });
   return stringToPrint;
